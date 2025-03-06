3e1b99a517a38666f8b41d5151d9a2ae875021d0
Stevenleez <128576233+Stevenleez@users.noreply.github.com>
2025-03-06 10:45:09 +0700

    Update BaseRepository.java

--- BaseRepository.java
+++ BaseRepository.java
@@ -1,8 +1,6 @@
 package com.pancabudi.common.common.misc;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
 
-@NoRepositoryBean
 public interface BaseRepository<T, ID> extends JpaRepository<T, ID> {
 }
