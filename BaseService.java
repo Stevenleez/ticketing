e8af36183fa31e413ceefe14396ddbc6ae32a3b1
Stevenleez <128576233+Stevenleez@users.noreply.github.com>
2025-03-06 10:47:14 +0700

    Update BaseService.java

--- BaseService.java
+++ BaseService.java
@@ -2,12 +2,9 @@ package com.pancabudi.common.common.misc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
 public abstract class BaseService<R extends JpaRepository<T, ID>, T, ID> {
 
     @Autowired
@@ -25,7 +22,7 @@ public abstract class BaseService<R extends JpaRepository<T, ID>, T, ID> {
         return repository.save(entity);
     }
 
-    public void deleteById(ID id) {
-        repository.deleteById(id);
-    }
+//    public void deleteById(ID id) {
+//        repository.deleteById(id);
+//    }
 }
